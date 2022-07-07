a59f6ecc835af8fc25dc502e8eb1fce91a1f9f1f
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2022-07-07 01:15:42 -0400

    Update generate_index_file.py

--- .github/py_repo_tools/generate_index_file.py
+++ .github/py_repo_tools/generate_index_file.py
@@ -42,11 +42,30 @@ output_file = "index.html"
 try:
     with codecs.open(output_file, 'w', encoding='utf-8') as f:
         f.write(f"""<head><title>{SiteTitle}</title>
-	   {SiteDescription}
-	     {Author}
+	
+	<!-- Primary Meta Tags -->
+
             <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="title" content="{SiteTitle}">
+<meta name="description" content="{SiteDescription}">
+
+<!-- Open Graph / Facebook -->
+<meta property="og:type" content="website">
+<meta property="og:url" content="https://{Author}.github.io/{SiteTitle}">
+<meta property="og:title" content="{SiteDescription}">
+<meta property="og:description" content="{SiteDescription}">
+<meta property="og:image" content="https://og-image.vercel.app/{SiteTitle}.png?theme=light&md=0&fontSize=100px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fhyper-color-logo.svg">
+
+<!-- Twitter -->
+<meta property="twitter:card" content="summary_large_image">
+<meta property="twitter:url" content="https://{Author}.github.io/{SiteTitle}">
+<meta property="twitter:title" content="{SiteTitle}">
+<meta property="twitter:description" content="{SiteDescription}">
+<meta property="twitter:image" content="https://og-image.vercel.app/{SiteTitle}.png?theme=light&md=0&fontSize=100px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fhyper-color-logo.svg">
+	   
+	 
 <script src="https://cdn.jsdelivr.net/npm/prismjs@1.28.0/prism.min.js"></script>   
      </head>""" + """
 <!--Body Content -->
