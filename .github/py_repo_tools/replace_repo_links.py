7447121b6e2a6a1e1264209b914b593131e8dad4
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2022-09-06 20:34:33 -0400

    Update replace_repo_links.py

--- .github/py_repo_tools/replace_repo_links.py
+++ .github/py_repo_tools/replace_repo_links.py
@@ -1,36 +1,7 @@
-# Python program to read
-# json file
-  
-  
-import json
-  
-# Opening JSON file
-f = open('.github/py_repo_tools/repo_config.json')
-  
-# returns JSON object as 
-# a dictionary
-config = json.load(f)
-  
+from html2image import Html2Image
+hti = Html2Image()
 
-# Define the filename here you want to replace content in
-FileName = "README.md"
+html = """<h1> An interesting title </h1> This page will be red"""
+css = "body {background: red;}"
 
-Text_To_Replace = config['Text_To_Replace']
-
-Text_To_Replace_With = config['Text_To_Replace_With']
-
-# Closing file
-f.close()
-
-
-# Open the File
-with open(FileName, 'r') as f:
-    # Read the file contents
-    contents = f.read()
-    # Replace the file contents
-    contents = contents.replace(Text_To_Replace, Text_To_Replace_With)
-
-# Write the file out    
-with open(FileName, 'w') as f:
-    # Write the updated contents
-    f.write(contents)  
+hti.screenshot(html_str=html, css_str=css, save_as='red_page.png')
